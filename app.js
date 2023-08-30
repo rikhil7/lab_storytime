fd8f793e19b59b466eefb874b41254da03e4f4e6
rikhil7 <rikhil.taneja@kalvium.community>
2023-08-30 17:53:00 +0530

    done

--- app.js
+++ app.js
@@ -2,17 +2,32 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
-function moreAboutHome(address,distanceFromTown,hasNeighbours){
-    return (typeof(address)+typeof(distanceFromTown)+typeof(hasNeighbours));
+function moreAboutHome(address, distanceFromTown, hasNeighbours) {
+    return (typeof (address) + typeof (distanceFromTown) + typeof (hasNeighbours));
 }
 //Trial 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    if (typeof (parents) == "string" && typeof (noOfSiblings) == "number" && typeof (isNuclearFamily) == "boolean") {
+        return true
+    } else {
+        return false
+    }
+}
+
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText, ageInNumber) {
+    if (isNaN(ageInText)) {
+        return ageInText
+    } else if (isNaN(ageInNumber)) {
+        return ageInNumber
+    }
+}
 
 //Trial 4:
 //Lily gave Karen x sweets
@@ -20,10 +35,18 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    var remaining = totalNoOfSweets-(sweetsConsumedByKaren+sweetsConsumedInNMeters*metersToTravel)
+    return remaining/2
+}
 
 //Trial 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelsius(fahrenheit){
+    var temperature = ((fahrenheit-32)*5/9)
+    return temperature
+}
 
 //Trial 6:
 //Lily can now do multiple things to deal with this
@@ -32,6 +55,18 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+function aDifficultChoice(choice){
+    switch(choice){
+        case 1:
+            return "Take her daughter to a doctor"
+        case 2:
+            return "Talk to her husband about it"
+        case 1:
+            return "Counsel her daughter herself"
+        case 1:
+            return "Lock her daughter in her room"
+    }
+}
 
 //Challenge 1:
 //Lily realized that she'd hurt her daughter
@@ -41,3 +76,6 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+function consoleKaren(strategies){
+    return strategies.join(" ").length
+}
